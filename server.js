0b149cec7f761e633894f6240f6767a8f41fcb48
BaccarSabri2 <115673916+BaccarSabri2@users.noreply.github.com>
2023-12-22 23:04:11 +0100

    lastet20

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+//require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
 const cors = require('cors');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -23,36 +26,12 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
-// Define the CORS options
-// const corsOptions = {
-//   origin: 'https://billboards.eth.limo', // Replace with your actual production domain
-//   methods: 'GET',
-//   optionsSuccessStatus: 204,
-// };
-
-// app.use(cors(corsOptions));
-
-// Serve static files from the build folder
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// // API endpoint to fetch cryptocurrency data
-// app.get('/api/cryptocurrency/listings/latest', async (req, res) => {
-//   try {
-//     const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
-//       headers: {
-//          'X-CMC_PRO_API_KEY': 'e0fd5e52-7a1f-4efa-82d1-13bd719dccba',
-//        // 'X-CMC_PRO_API_KEY': process.env.REACT_APP_API_KEY,
-//       },
-//     });
-
-
-
 
 
 // API endpoint to fetch cryptocurrency data
 app.get('/api/cryptocurrency/listings/latest', async (req, res) => {
     try {
-        const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+        const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=20', {
             headers: {
                 'x-cmc_pro_api_key': 'e0fd5e52-7a1f-4efa-82d1-13bd719dccba',
             },
@@ -61,12 +40,14 @@ app.get('/api/cryptocurrency/listings/latest', async (req, res) => {
         return res.json(response.data);
     } catch (error) {
         console.error('Error fetching cryptocurrency data:', error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send('Internal Server Error!');
     }
 });
 
 // Catch-all route to serve the React app
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
